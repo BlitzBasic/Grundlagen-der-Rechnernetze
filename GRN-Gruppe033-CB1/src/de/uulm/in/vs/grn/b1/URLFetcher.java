0a35edfe7904e8783d94b24a62fe68dc8945f36e
Marius Vollbrecht <marius.vollbrecht@uni-ulm.de>
2016-11-24 20:42:17 +0100

    Completed Parsing, should work now. Testing required

--- GRN-Gruppe033-CB1/src/de/uulm/in/vs/grn/b1/URLFetcher.java
+++ GRN-Gruppe033-CB1/src/de/uulm/in/vs/grn/b1/URLFetcher.java
@@ -3,57 +3,51 @@ package de.uulm.in.vs.grn.b1;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URL;
 
 public class URLFetcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 		if (args[0] == null) {
 			System.err.println("Please insert an URL!");
 			System.exit(-1);
 		}
+
 		
-		//TODO: Complete splitting input into URL, extention, ...
-		String link = args[0];
-		if(link.startsWith("http://")) link=link.substring(6);
-		try{
-		int extensionIndex;
-		if((extensionIndex = link.lastIndexOf('.')) == -1) throw new MalformedURLException();
-		String filename = link.substring(link.lastIndexOf('/'), extensionIndex);
-		String extention = link.substring(extensionIndex);
-		System.out.println(filename + " . " + extention);
-		try (Socket socket = new Socket(args[0], 80);
-				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
+		try {
+			
+			URL url = new URL(args[0]);
 
-			writer.write("GET "+" HTTP/1.1\r\nHost: www.boersen-und-maerkte.de\r\n\r\n");
-			writer.flush();
+			//open connection
+			try (Socket socket = new Socket(url.getHost(), 80);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
 
-			String statusCode = reader.readLine().split(" ", 4)[1];
-			System.out.println(statusCode);
-			if (statusCode.equals("200")) {
-				try (BufferedWriter fileWriter = new BufferedWriter(
-						new OutputStreamWriter(new FileOutputStream(new File(filename + "." + extention))))) {
-					while (!reader.readLine().equals("")) ; // throw away header
-					while (reader.ready()) {
+				writer.write("GET " + url.getPath() + " HTTP/1.1\r\nHost: "+url.getHost()+"\r\n\r\n");
+				writer.flush();
 
-						fileWriter.write(reader.readLine()+"\r\n");
+				//extract status code
+				String statusCode = reader.readLine().split(" ", 4)[1];
+				
+				//read and write if everything is ok
+				if (statusCode.equals("200")) {
+					try (BufferedWriter fileWriter = new BufferedWriter(
+							new OutputStreamWriter(new FileOutputStream(new File(url.getFile().substring(1)))))) {
+						while (!reader.readLine().equals("")); // throw away header
+						while (reader.ready())	fileWriter.write(reader.readLine() + "\r\n");
+						fileWriter.flush();
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
 
-					fileWriter.flush();
-				} catch (Exception e) {
-					System.err.println("Scheiße");
 				}
-
 			}
-		}
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("The URL is not well-formatted");
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			System.err.println("Couldn't write file");
 		}
 
 	}
