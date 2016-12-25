6943f1b7808cd9274ebf62984ee3c078896e1650
Marius Vollbrecht <marius.vollbrecht@uni-ulm.de>
2016-12-25 01:24:28 +0100

    starting CC2

---
+++ GRN-Gruppe033-CC2/src/de/uulm/in/vs/grn/sockagram/server/ThreadedSever.java
@@ -0,0 +1,41 @@
+package de.uulm.in.vs.grn.sockagram.server;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadedSever {
+	private static final int PORT = 7777;
+ 
+	public static void main(String[] args) {
+
+		
+		try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+			ExecutorService pool = Executors.newFixedThreadPool(1);
+
+			try {
+				
+				while (true) {
+					pool.execute(new SockagramRequestHandler(serverSocket.accept()));	//wait for a connecting host, start new thread for each
+				}
+				
+			//catch exceptions
+			} catch (BindException bindError) {
+				System.err.println("BindError! (Vielleicht läuft letzte Instanz des Programms noch)");
+				System.exit(1);
+			} catch (IOException iOErrorCon) {
+				iOErrorCon.printStackTrace();
+			}
+
+		//exit the program on serious exceptions
+		} catch (Exception e) {
+			System.err.println("Es gab einen unbekannten Fehler");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+	}
+}
