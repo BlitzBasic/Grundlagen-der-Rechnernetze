c35a312790e18b4d7ec18695caef2ee508f567bb
Marius Vollbrecht <marius.vollbrecht@uni-ulm.de>
2017-01-13 20:18:02 +0100

    merged example code and ours, now ready to use event objects

--- GRN-Gruppe033-CE2/src/de/uulm/in/vs/grn/chat/client/CommandCommunicator.java
+++ GRN-Gruppe033-CE2/src/de/uulm/in/vs/grn/chat/client/CommandCommunicator.java
@@ -2,7 +2,7 @@ package de.uulm.in.vs.grn.chat.client;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FilterInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
@@ -13,11 +13,13 @@ public class CommandCommunicator extends Thread {
 	
 	private InetAddress address;
 	private int port;
+	private boolean active = false;
 
 	public CommandCommunicator(InetAddress address, int port) {
 		super();
 		this.address = address;
 		this.port = port;
+		active = true;
 	}
 	
 	public void run(){
@@ -26,83 +28,14 @@ public class CommandCommunicator extends Thread {
 				BufferedWriter commandSocketWriter = new BufferedWriter(new OutputStreamWriter(commandSocket.getOutputStream()));
 				BufferedReader inputReader = new BufferedReader(new InputStreamReader(commandSocket.getInputStream()))) {
 
-			while (true) {
-				try {
-					
-					while(true){
-						String inputLine = inputReader.readLine();
-					}
-					
-					
-					
-					
-					//first write, than receive
-					
-//					// read first line of message
-//					String line = comReader.readLine();
-//					String[] parameters = line.split(" ", 2);
+			while (active) {
+//				try {
+//					
+//					
 //
-//					String protocol = parameters[0];
-//					CommandExpression commandExp = CommandExpression.valueOf(parameters[1]);
-//
-//					// Check for the implemented protocol version
-//					if (protocol.equals(GRNCP.PROTOCOL_VERSION)) {
-//
-//						// choose the correct message type
-//						switch (commandExp) {
-//
-//						case MESSAGE: {
-//							// read date
-//							line = comReader.readLine();
-//							String date = line.substring(line.indexOf(' ') + 1);
-//
-//							// read name
-//							line = comReader.readLine();
-//							String username = line.substring(line.indexOf(' ') + 1);
-//
-//							// read text
-//							line = comReader.readLine();
-//							String text = line.substring(line.indexOf(' ') + 1);
-//
-//							// read empty line
-//							comReader.readLine();
-//
-//							System.out.println(date + " | " + username + ": " + text);
-//							break;
-//						}
-//
-//						case EVENT: {
-//							// read date
-//							line = comReader.readLine();
-//							String date = line.substring(line.indexOf(' ') + 1);
-//
-//							// read description
-//							line = comReader.readLine();
-//							String description = line.substring(line.indexOf(' ') + 1);
-//
-//							// read empty line
-//							comReader.readLine();
-//
-//							System.out.println(date + " | " + description);
-//							break;
-//						}
-//
-//						default:
-//							// ignore
-//							break;
-//						}
-//
-//					}
-
-				} catch (IllegalArgumentException e) {
-					// do nothing
-				} catch (ArrayIndexOutOfBoundsException e) {
-					// do nothing
-				} catch (IndexOutOfBoundsException e) {
-					// do nothing
-				} catch (NullPointerException e) {
-					// do nothing
-				}
+//				} catch (IOException e) {
+//					// do nothing
+//				}
 
 			}
 
--- GRN-Gruppe033-CE2/src/de/uulm/in/vs/grn/chat/client/PubListener.java
+++ GRN-Gruppe033-CE2/src/de/uulm/in/vs/grn/chat/client/PubListener.java
@@ -9,11 +9,13 @@ public class PubListener extends Thread {
 
 	private InetAddress address;
 	private int port;
+	private boolean active = false;
 
 	public PubListener(InetAddress address, int port) {
 		super();
 		this.address = address;
 		this.port = port;
+		active=true;
 	}
 
 	@Override
@@ -26,63 +28,108 @@ public class PubListener extends Thread {
 				BufferedReader pubReader = new BufferedReader(new InputStreamReader(pubSocket.getInputStream()))) {
 			System.out.println("You are connected!");
 
-			while (true) {
+			while (active) {
 				try {
-					// read first line of message
-					String line = pubReader.readLine();
-					String[] parameters = line.split(" ", 2);
-
-					String protocol = parameters[0];
-					CommandExpression commandExp = CommandExpression.valueOf(parameters[1]);
-
-					// Check for the implemented protocol version
-					if (protocol.equals(GRNCP.PROTOCOL_VERSION)) {
-
-						// choose the correct message type
-						switch (commandExp) {
-
-						case MESSAGE: {
-							// read date
-							line = pubReader.readLine();
-							String date = line.substring(line.indexOf(' ') + 1);
-
-							// read name
-							line = pubReader.readLine();
-							String username = line.substring(line.indexOf(' ') + 1);
-
-							// read text
-							line = pubReader.readLine();
-							String text = line.substring(line.indexOf(' ') + 1);
-
-							// read empty line
-							pubReader.readLine();
-
-							System.out.println(date + " | " + username + ": " + text);
-							break;
-						}
-
-						case EVENT: {
-							// read date
-							line = pubReader.readLine();
-							String date = line.substring(line.indexOf(' ') + 1);
-
-							// read description
-							line = pubReader.readLine();
-							String description = line.substring(line.indexOf(' ') + 1);
-
-							// read empty line
-							pubReader.readLine();
-
-							System.out.println(date + " | " + description);
-							break;
-						}
-
-						default:
-							// ignore
-							break;
-						}
-
-					}
+					
+					String response = "";
+					
+					String command = "";
+					String date = "";
+					String username = "";
+					String text = "";
+					String description = "";
+					String[] field;
+					do {
+					    response = pubReader.readLine();
+//					    System.out.println(response);
+					    if (response.split(" ")[0].equals("GRNCP/0.1")){
+					    	command = response.split(" ")[1];
+					    } else if ((field = response.split(": ")).length > 1){
+					    	if (field[0].equals("Date"))
+								date = field[1];
+							else if (field[0].equals("Username"))
+						    	username = field[1].trim();
+						    else if (field[0].equals("Text"))
+						    	text = field[1].trim();
+						    else if (field[0].equals("Description"))
+						    	description = field[1].trim();
+					    } else if (response.equals("")){
+					    	switch (command){
+							case "MESSAGE": 
+								//TODO: construct event
+								System.out.println(date + " | " + username + ": " + text);
+											command = "";
+											break;
+							case "EVENT":	
+								//TODO: construct event
+								System.out.println(date + " | " + description);
+											command = "";
+											break;
+							}
+					    }
+					} while (response != null);
+					
+					
+					
+					
+					
+					
+//					//old
+//					// read first line of message
+//					String line = pubReader.readLine();
+//					String[] parameters = line.split(" ", 2);
+//
+//					String protocol = parameters[0];
+//					CommandExpression commandExp = CommandExpression.valueOf(parameters[1]);
+//
+//					// Check for the implemented protocol version
+//					if (protocol.equals(GRNCP.PROTOCOL_VERSION)) {
+//
+//						// choose the correct message type
+//						switch (commandExp) {
+//
+//						case MESSAGE: {
+//							// read date
+//							line = pubReader.readLine();
+//							String date = line.substring(line.indexOf(' ') + 1);
+//
+//							// read name
+//							line = pubReader.readLine();
+//							String username = line.substring(line.indexOf(' ') + 1);
+//
+//							// read text
+//							line = pubReader.readLine();
+//							String text = line.substring(line.indexOf(' ') + 1);
+//
+//							// read empty line
+//							pubReader.readLine();
+//
+//							System.out.println(date + " | " + username + ": " + text);
+//							break;
+//						}
+//
+//						case EVENT: {
+//							// read date
+//							line = pubReader.readLine();
+//							String date = line.substring(line.indexOf(' ') + 1);
+//
+//							// read description
+//							line = pubReader.readLine();
+//							String description = line.substring(line.indexOf(' ') + 1);
+//
+//							// read empty line
+//							pubReader.readLine();
+//
+//							System.out.println(date + " | " + description);
+//							break;
+//						}
+//
+//						default:
+//							// ignore
+//							break;
+//						}
+//
+//					}
 
 				} catch (IllegalArgumentException e) {
 					// do nothing
