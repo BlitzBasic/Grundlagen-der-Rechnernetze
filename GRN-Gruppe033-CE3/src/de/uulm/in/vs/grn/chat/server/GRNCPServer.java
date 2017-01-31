bc835004a6d5e8e868f6280d5438edab51984d7d
Marius Vollbrecht <marius.vollbrecht@uni-ulm.de>
2017-01-31 15:39:23 +0100

    changes today

--- GRN-Gruppe033-CE3/src/de/uulm/in/vs/grn/chat/server/GRNCPServer.java
+++ GRN-Gruppe033-CE3/src/de/uulm/in/vs/grn/chat/server/GRNCPServer.java
@@ -1,6 +1,10 @@
 package de.uulm.in.vs.grn.chat.server;
 
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import de.uulm.in.vs.grn.chat.server.command.CommandAcceptor;
 import de.uulm.in.vs.grn.chat.server.pubsub.PubSubAcceptor;
@@ -16,6 +20,7 @@ public class GRNCPServer {
 	public static final String PROTOCOL_VERSION = "GRNCP/0.1";
 	public static final int COMMAND_PORT = 8122;
 	public static final int PUB_SUB_PORT = 8123;
+	public static final DateTimeFormatter DATIMINATOR = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss");
 
 	public static void main(String[] args) {
 
@@ -33,7 +38,11 @@ public class GRNCPServer {
 		PubSubHandlerGroup pubSubHandlerGroup = new PubSubHandlerGroup();
 		PubSubAcceptor pubSubAcceptor = new PubSubAcceptor(pubSubPort, pubSubHandlerGroup);
 		CommandAcceptor commandAcceptor = new CommandAcceptor(commandPort, pubSubHandlerGroup);
-
+		ExecutorService pool = Executors.newFixedThreadPool(3);
+		pool.submit(pubSubHandlerGroup);
+		pool.submit(pubSubAcceptor);
+		pool.submit(commandAcceptor);
+		
 	}
 
 }
--- GRN-Gruppe033-CE3/src/de/uulm/in/vs/grn/chat/server/command/CommandHandler.java
+++ GRN-Gruppe033-CE3/src/de/uulm/in/vs/grn/chat/server/command/CommandHandler.java
@@ -1,14 +1,32 @@
 package de.uulm.in.vs.grn.chat.server.command;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import de.uulm.in.vs.grn.chat.server.GRNCPServer;
 import de.uulm.in.vs.grn.chat.server.PubSubHandlerGroup;
+import de.uulm.in.vs.grn.chat.server.messages.events.Event;
+import de.uulm.in.vs.grn.chat.server.messages.events.GRNCPMessageEvent;
+import de.uulm.in.vs.grn.chat.server.messages.requests.GRNCPSend;
+import de.uulm.in.vs.grn.chat.server.messages.responses.GRNCPLoggedin;
+import de.uulm.in.vs.grn.chat.server.messages.responses.GRNCPSent;
+import de.uulm.in.vs.grn.chat.server.messages.responses.Response;
 
 public class CommandHandler implements Runnable {
 
 	private Socket socket;
 	private PubSubHandlerGroup handlerGroup;
 	boolean active = false;
+	boolean loggedin = false;
+	String username;
+	
+	
 
 	CommandHandler(Socket socket, PubSubHandlerGroup handlerGroup) {
 		this.socket = socket;
@@ -18,8 +36,96 @@ public class CommandHandler implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+					// 1. check login state
+					// 2. wait for request
+					// 3. forward to handlerGroup
+					// 4. send response
+				
+		
+		try (BufferedReader commandReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				BufferedWriter commandWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+
+			while (active) {
+				try {
+
+					String request = "";
+					String[] field;
+					String command = "";
+					String username = "";
+					String text = "";
+					String date = "";
+					Response response = null;
+					do {
+						request = commandReader.readLine();
+						if (request.split(" ")[1].equals("GRNCP/0.1")) {
+							command = request.split(" ")[0];
+						} else if ((field = request.split(": ")).length > 1) {
+							if (field[0].equals("Username")){
+								username = field[1].trim();
+							}else if (field[0].equals("Text")){
+								text = field[1].trim();
+							}
+						} else if (request.equals("")) {
+							date = (LocalDateTime.now()).format(GRNCPServer.DATIMINATOR);
+							switch (command) {
+							case "LOGIN":
+								//TODO: Peter regex schreiben
+								response = new GRNCPLoggedin(date);
+								command = "";
+								break;
+							case "PING":
+								//nothing
+								command = "";
+								break;
+							case "BYE":
+								//nothing
+								// TODO
+								command = "";
+								break;
+							case "SEND":
+								// TODO Peter conventions
+								Event event = new GRNCPMessageEvent(date, username, text);
+								response = new GRNCPSent(date);
+								
+								command = "";
+								break;
+							default:
+								//nothing
+								// TODO
+								command = "";
+								break;
+							}
+
+						}
+					} while (request != null);
+					
+					if(response!=null){
+						response.send(commandWriter);
+						//send response
+					}
+
+				} catch (ArrayIndexOutOfBoundsException e) {
+					// do nothing
+				} catch (IndexOutOfBoundsException e) {
+					// do nothing
+				} catch (NullPointerException e) {
+					// do nothing
+				} catch (SocketException e) {
+					active = false;
+					//TODO
+				}
+
+			}
+
+		} catch (Exception e) {
+			// TODO
+		}
+		
+		
 		while (active) {
+			while(!loggedin){
+				
+			}
 			// 1. check login state
 			// 2. wait for request
 			// 3. forward to handlerGroup
